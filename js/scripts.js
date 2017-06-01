90220caae26bd0d16a15d4befe9c0dffd9a39077
Brian Rush and Calvin Cushman <brian+calvin@>
2017-06-01 11:02:48 -0700

    completes the youtube tutorial at watch?v=Ct7APU7t-ts

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+$(function(){
+  alert("loaded!");
+  // Select all elements with data-toggle="popover" in the document
+  $('[data-toggle="popover"]').popover();
+
+  // Select a specified element
+  $('#myPopover').popover();
+});
